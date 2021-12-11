11609386aca8de44da4141143e3cf6a3844fa13d
zexy-swami <my.zexy.swami@gmail.com>
2021-12-11 17:25:11 +0300

    Adding encoding process

---
+++ internal/encoding/encoding.go
@@ -0,0 +1,27 @@
+package encoding
+
+import (
+	"errors"
+	"fmt"
+	"math/bits"
+
+	data "golay_code/data"
+)
+
+const maxValueToEncode = uint16((1 << 12) - 1)
+
+func Encoding(valueToEncode uint16) (uint32, error) {
+	if valueToEncode > maxValueToEncode {
+		valueRangeError := fmt.Sprintf("[encoding/ValueEncoding]: Value to encode %d, expected values are [0, 4095]", valueToEncode)
+		return 0, errors.New(valueRangeError)
+	}
+
+	var encodedValue = uint32(valueToEncode) << 11
+
+	for i := 12; i < 23; i++ {
+		if onesCount := bits.OnesCount16(valueToEncode & data.GT[i]); onesCount%2 == 1 {
+			encodedValue += uint32(1) << (22 - i)
+		}
+	}
+	return encodedValue, nil
+}
